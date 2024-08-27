a301d798641e8b935300746453b66d8963d2ad2c
DK <111112592+onegood07@users.noreply.github.com>
2024-08-27 16:05:42 +0900

    Update 02_SumNumbers.py
    오탈자 수정

--- Numeric/02_SumNumbers.py
+++ Numeric/02_SumNumbers.py
@@ -1,6 +1,6 @@
 # 02. 숫자 더하기
 
-def making_sum(*numbers): # *(가변 인수)는 몇 개의 인자를 받을 건지 미리 설정하지 않고도 모든 경우를 처리 가능함, 모든 인수를 튜플 형태로 묶음
+def making_sum(*numbers): # *(전개 연산자)는 몇 개의 인자를 받을 건지 미리 설정하지 않고도 모든 경우를 처리 가능함, 모든 인수를 튜플 형태로 묶음
     # sum 함수와 같은 역할 수행
     result = 0
     for number in numbers: 
