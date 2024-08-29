547a7c1dccf697dd65d546ab2af7e775b43eeed4
KDK <one_good07@naver.com>
2024-08-29 16:53:20 +0900

    10번 추가문제 풀기

--- ListAndTuple/10_AddElements.py
+++ ListAndTuple/10_AddElements.py
@@ -42,4 +42,19 @@ def add_elements_bigger_than(big_element, *args):
 # 실행
 print(add_elements_bigger_than(10, 5, 20, 30, 6))
 print(add_elements_bigger_than('c', 'd', 'c', 'b', 'h'))
+
+
+# 함수 - 여러 개의 매개변수를 받아 정수 또는 정수로 변환할 수 있는 값만 더해서 리턴하는 함수
+def sum_numeric(*args):
+    output = 0
+
+    for item in args:
+        if type(item) == int:
+            output += item
+        elif item.isdigit():
+            output += int(item)
     
+    return output
+
+print(sum_numeric(10, 20, 'a', '30', 'bcd'))
+print(sum_numeric(1, '3', 'ad'))
