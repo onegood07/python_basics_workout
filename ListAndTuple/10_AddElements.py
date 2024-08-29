deaf9e119c95904fce63240d53f4d82105feee11
KDK <one_good07@naver.com>
2024-08-29 16:47:01 +0900

    10번 문제 풀기

---
+++ ListAndTuple/10_AddElements.py
@@ -0,0 +1,45 @@
+# 10. 아무거나 더하기
+
+# 함수 - + 연산자를 갖는 자료형(숫자, 문자열, 리스트, 튜플 등)을 매개변수로 받게 확장한 함수
+def add_elements(*args):
+    if not args:
+        return args
+    
+    ouput = args[0]
+    for item in args[1:]:
+        ouput += item
+    
+    return ouput
+
+# 실행
+# print(add_elements())
+# print(add_elements(10, 20, 30, 40))
+# print(add_elements('a', 'b', 'c', 'd'))
+# print(add_elements([10, 20, 30], [40, 50, 60], [70, 80, 90]))
+
+
+# 함수 - add_elements와 비슷하고, *args 앞에 매개변수를 하나 더 받아, 그 매개변수보다 큰 요소만 더해서 리턴하는 함수
+def add_elements_bigger_than(big_element, *args):
+    count = len(args)
+    start_index = 0
+
+    # 가장 처음으로 big_element보다 큰 요소 찾기 (시작점)
+    while count:
+        for index, item in enumerate(args):
+            count -= 1
+            if big_element < item:
+                output = item
+                start_index = index
+                break
+           
+    # 해당 요소부터 시작해 big_element보다 크면 더하기
+    for item in args[start_index + 1:]:
+        if big_element < item:
+            output += item
+    
+    return output
+
+# 실행
+print(add_elements_bigger_than(10, 5, 20, 30, 6))
+print(add_elements_bigger_than('c', 'd', 'c', 'b', 'h'))
+    
