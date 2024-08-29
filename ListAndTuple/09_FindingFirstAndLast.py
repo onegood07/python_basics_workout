cb13aa3787395a8fa6f47896226cdf14fa9ebde3
KDK <one_good07@naver.com>
2024-08-29 15:47:46 +0900

    수정

--- ListAndTuple/09_FindingFirstAndLast.py
+++ ListAndTuple/09_FindingFirstAndLast.py
@@ -44,19 +44,22 @@ def plus_minus(input_ints):
 # print(plus_minus([10, 20, 30, 40, 50, 60]))
 
 
-# 함수 - 
-def test(*data):
-    output_list = []
+# 함수 - 파이썬이 기본 제공하는 zip 함수 직접 구현하기
+# 매개변수로 여러 개의 이터러블을 받고 튜플로 구성된 리스트를 반환, 각각 튜플은 매개변수로 받은 이터러블의 해당 인덱스 요소를 조합해서 만들어짐
+def my_zip_func(*data):
+    # '리스트 내포'를 활용하여 빈 리스트 만들기 -> 추후 튜플로 변경 (append를 활용하려고 리스트로 만듬)
     empty_list = [[] for _ in range(len(data[0]))]
+    output_list = []
 
-    for one_data in data: # 이터러블 하나씩 받는 거임 [10, 20, 30] 이런 식으로
-        for index, one_thing in enumerate(one_data): # [10, 20, 30] 안의 10
+    for one_data in data: 
+        for index, one_thing in enumerate(one_data):  # emumerate()를 활용해서 index도 동시에 사용
             empty_list[index].append(one_thing)
     
-    for index in range(len(empty_list)):
+    # 튜플로 구성된 리스트 만들기
+    for index in range(len(empty_list)): 
         output_list.append(tuple(empty_list[index]))
 
     return output_list
 
 # 실행
-print(test([10, 20, 30], 'abc'))
+print(my_zip_func([10, 20, 30], 'abc'))
