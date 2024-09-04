8b22079a11c8c00a80cc3b435ebf57d10430c0f3
KDK <one_good07@naver.com>
2024-09-04 10:19:14 +0900

    19번

--- File/19_PasswdToDic.py
+++ File/19_PasswdToDic.py
@@ -1,5 +1,7 @@
 # 19. /etc/passwd를 딕셔너리로 바꾸기
 
+# [메인 문제]
+# 키로 사용자 이름을 갖고, 값으로 사용자 ID를 갖는 딕녀서리 지정하기
 def passwd_to_dic():
     output_dic = {}
 
@@ -11,14 +13,12 @@ def passwd_to_dic():
 
     return output_dic
 
-# print(passwd_to_dic())
+# 실행
+print(passwd_to_dic())
 
 
-# nobody:*:-2:-2::0:0:Unprivileged User:/var/empty:/usr/bin/false
-# 로그인 셸 (Login Shell): /usr/bin/false
-# 사용자 ID (UID): -2
-# 홈 디렉터리 (Home Directory): /var/empty
-# 셀 (Shell): 로그인 셸과 동일하게 /usr/bin/false
+# [추가 문제]
+# 키로 사용자 이름을 갖고, 값으로 사용자 ID, 홈 디렉터리, 셸을 갖는 딕셔너리 지정하기
 
 def passwd_to_dic_having_more_info(filename):
     users = {}
@@ -26,10 +26,10 @@ def passwd_to_dic_having_more_info(filename):
     with open("/Users/kdk/Desktop/passwd.txt") as file:
         for line in file:
             if not line.startswith(('#', '\n')):
-                user_info = line.strip()
-                user_info = user_info.split(':')
+                user_info = user_info.strip().split(':')
                 users[user_info[0]] = [user_info[2], user_info[-2], user_info[-1]]
         
     return users
 
+# 실행
 print(passwd_to_dic_having_more_info('/etc/passwd'))
